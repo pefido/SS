722d872eb893ad32734b39d2ce82d6c2ac4c0bcd
joaocampinhos <joao.campinhos@gmail.com>
2015-10-26 18:14:37 +0000

    imports

--- src/web/ApplicationServlet.java
+++ src/web/ApplicationServlet.java
@@ -1,15 +1,12 @@
 package web;
 
 import java.io.*;
-import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.omg.Messaging.SyncScopeHelper;
 
 import controller.Authenticator;
 import util.Template;
